f6263761396c071110169f31bb2d6df2d0c8db2b
Venkat <venkat.jangari@gmail.com>
2018-03-10 07:34:29 -0500

    added router and firebase deployment

---
+++ src/app/app-routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { ManageProductsComponent } from './admin/manage-products/manage-products.component';
+import { ManageOrdersComponent } from './admin/manage-orders/manage-orders.component';
+import { LogoutComponent } from './logout/logout.component';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'shopping-cart',        component: ShoppingCartComponent },
+    { path: 'checkout', component: CheckoutComponent },
+    { path: 'my-orders', component: MyOrdersComponent },
+    { path: 'products', component: ProductsComponent },
+    
+    { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
+
+    { path: 'admin/products', component: ManageProductsComponent },
+    { path: 'admin/orders', component: ManageOrdersComponent },
+
+    { path: '**', component: HomeComponent }
+  ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AngularFireModule } from 'angularfire2';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProductsComponent } from './products/products.component';
+import { ManageProductsComponent } from './admin/manage-products/manage-products.component';
+import { ManageOrdersComponent } from './admin/manage-orders/manage-orders.component';
+import { LoginComponent } from './login/login.component'
 
-
+import { appRoutes } from './app-routes';
+import { LogoutComponent } from './logout/logout.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    MyOrdersComponent,
+    CheckoutComponent,
+    ProductsComponent,
+    ManageProductsComponent,
+    ManageOrdersComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
-    BrowserModule
+ 
+  BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot(),
+    AngularFireModule
   ],
   providers: [],
   bootstrap: [AppComponent]
